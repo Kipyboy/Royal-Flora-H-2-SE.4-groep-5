b3c8d141320f26282152d51ff01a4bfa2590cbb4
alexander <24094714@student.hhs.nl>
2025-11-11 14:16:26 +0100

    topbar is een component, moet nog toevoegen dat er een hamburger menu voor sidebar komt op paginas die dat willen en mss pfp foto van user krijgen

--- royal-flora-frontend/src/app/accountDetails/page.tsx
+++ royal-flora-frontend/src/app/accountDetails/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';    
 import '../../styles/AccountDetails.css';
 import { useRouter } from 'next/navigation';
+import Topbar from '../components/Topbar';
 
 interface UserDetails {
     voornaam: string;
@@ -87,27 +88,13 @@ const AccountDetails: React.FC = () => {
 
     return (
         <>
-              <nav className="nav">
-                <div className="left">
-                    <span className="nav-text">Account details</span>
-                </div>
-                <div className="nav-logo-container">
-                <a href="/homepage" className="nav-logo-link" aria-label="Ga naar homepagina" onClick={() => router.push('/homepage')}>
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Royal_FloraHolland_Logo.svg/1200px-Royal_FloraHolland_Logo.svg.png"
-                        alt="Royal FloraHolland Logo"
-                        className="nav-logo"
-                    />
-                </a>
-                </div>
-                <a className="pfp-container" href="#">
-                <img
-                    src="https://www.pikpng.com/pngl/m/80-805068_my-profile-icon-blank-profile-picture-circle-clipart.png"
-                    alt="Profiel"
-                    className="pfp-img"
-                />
-                </a>
-            </nav>
+            
+            <Topbar
+                useSideBar={false}
+                currentPage="Account Details"
+                currentUser="User"
+            />
+            
 
             <main id="main">
                 <form onSubmit={e => e.preventDefault()} aria-labelledby="accountdetails-title">
