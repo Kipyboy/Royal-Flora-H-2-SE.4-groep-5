ba1a48074cc8cc2d2f01f7e39c63be24209c6b78
BjornS2006 <24074918@student.hhs.nl>
2025-11-26 17:11:19 +0100

    status changes
    status changes plus kleine verandering aan de homepage

--- RoyalFlora/Controllers/ProductsController.cs
+++ RoyalFlora/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MySqlX.XDevAPI;
 using RoyalFlora.Migrations;
 
@@ -47,6 +48,7 @@ namespace RoyalFlora.Controllers
         {
             List<Product> products = await _context.Products
             .Include(p => p.LeverancierNavigation)
+            .Include(p => p.StatusNavigation)
             .ToListAsync();
             List<ProductDTO> productDTOs = new List<ProductDTO>();
            
@@ -71,6 +73,7 @@ namespace RoyalFlora.Controllers
                 };
                 productDTOs.Add(dto);
             }
+            
             // Debug output
             foreach (var dto in productDTOs)
             {
@@ -136,11 +139,18 @@ namespace RoyalFlora.Controllers
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
+            images = new List<IFormFile>();
+
             _context.Fotos.AddRange(images.Select(image => new Foto
             {
                 IdProduct = product.IdProduct,
                 FotoPath = image.FileName 
             }));
+
+            if (images.IsNullOrEmpty())
+                {
+                    Console.WriteLine("het werkt niet lol");
+                }
 
             await _context.SaveChangesAsync();
             return Ok(product);
