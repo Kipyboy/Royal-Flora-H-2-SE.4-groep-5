a824b84eabecc43a19dbf495703e6d0eeabd2acc
Alexander de Weger <24094714@student.hhs.nl>
2026-01-06 11:32:20 +0100

    voor veilingmeester delete veiling ding gemaakt en kan registreren als veilingmeester

---
+++ royal-flora-frontend/src/app/components/veilingmeester-product-card.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import '../../styles/ProductCard.css';
+import { API_BASE_URL } from '../config/api';
+import { authFetch } from "../utils/api";
+
+
+interface VeilingmeesterProductCardProps {
+    id: number;
+    naam: string;
+    merk: string;
+    prijs: string | number;
+    datum: string;
+    locatie: string;
+    status: string;
+    aantal: number;
+    fotoPath: string;
+    beschrijving: string;
+    toonBeschrijving: boolean;
+}
+
+const handleDelete = async (id: number) => {
+    // Implement delete functionality here
+    try{
+        const response = await authFetch(
+            `${API_BASE_URL}/api/Products/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+        
+        if (response.ok) {
+            alert("Product deleted successfully");
+        }
+    }
+    catch(error){
+        alert("Error deleting product");
+    }
+    return;
+}
+
+
+const VeilingmeesterProductCard: React.FC<VeilingmeesterProductCardProps> = ({
+    id,
+    naam,
+    merk,
+    prijs,
+    datum,
+    locatie,
+    status,
+    aantal,
+    fotoPath
+    , beschrijving,
+    toonBeschrijving
+}) => {
+    
+    const defaultImg = "https://syria.adra.cloud/wp-content/uploads/2021/10/empty.jpg";
+    return (
+    <div className="product-card">
+        <div className="image">
+            <img src={fotoPath && fotoPath.trim() !== "" ? `${API_BASE_URL}/images/${fotoPath}` : defaultImg} alt="" id="product-foto"/>
+        </div>
+        <div className="info" style={{ display : toonBeschrijving ? 'none' : 'block'}}>
+            <p id="naam">Naam: {naam}</p>
+            <p id="merk">Merk: {merk}</p>
+            <p id="aantal">Aantal: {aantal}</p>
+            <p id="prijs">Prijs: {prijs}</p>
+            <p id="datum">Datum: {datum}</p>
+            <p id="locatie">Locatie: {locatie}</p>
+            <p id="status">Status: {status}</p>
+        </div>
+        <div className="beschrijving" style={{ display: toonBeschrijving ? 'block' : 'none' }}>
+            {beschrijving}
+        </div>
+        <button className="delete-button" onClick={() => handleDelete(id)}>Delete</button>
+    </div>
+    );
+};
+
+export default VeilingmeesterProductCard;
