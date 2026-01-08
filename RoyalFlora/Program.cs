4a2d2428380b44b1502a1b164e4a99e0620c197e
Jorn <jornkamps@outlook.com>
2026-01-08 03:30:40 +0100

    was een comma vergeten

--- RoyalFlora/Program.cs
+++ RoyalFlora/Program.cs
@@ -61,11 +61,10 @@ namespace RoyalFlora
                     policy.WithOrigins(
                               "http://localhost:3000",
                               "http://80.56.53.41:3000",
-                              "https://chicken.servegame.com/"  // Vervang met je No-IP domein
+                              "https://chicken.servegame.com/",  // Vervang met je No-IP domein
                               "https://localhost:3000",
                               "http://127.0.0.1:3000",
                               "https://127.0.0.1:3000",
-                              "http://80.56.53.41:3000"
                               "http://80.56.53.41:3000",
                               "https://chicken.servegame.com/"  // Vervang met je No-IP domein
                           )
