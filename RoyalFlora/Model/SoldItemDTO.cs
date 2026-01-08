cbd9aaa70529712116412c8052b04c4b74aa2e68
Jorn <jornkamps@outlook.com>
2026-01-08 12:26:13 +0100

    tijdelijk possible null

--- RoyalFlora/Model/SoldItemDTO.cs
+++ RoyalFlora/Model/SoldItemDTO.cs
@@ -1,7 +1,7 @@
 public class SoldItemDTO
 {
     public int IdProduct { get; set; }
-    public string ProductNaam { get; set; } = string.Empty;
-    public decimal VerkoopPrijs { get; set; }
+    public string? ProductNaam { get; set; }
+    public decimal? VerkoopPrijs { get; set; }
     public int? Aantal { get; set; }
 }
