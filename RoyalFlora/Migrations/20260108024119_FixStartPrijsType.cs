8717317cb06c494bd20c0d232741a4ef69f225a2
Jorn <jornkamps@outlook.com>
2026-01-08 03:50:30 +0100

    fixing database met migrations?

---
+++ RoyalFlora/Migrations/20260108024119_FixStartPrijsType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoyalFlora.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStartPrijsType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StartPrijs",
+                table: "Products",
+                type: "decimal(10,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StartPrijs",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldNullable: true);
+        }
+    }
+}
