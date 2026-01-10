09e4b0f44761ebc439d738e460e479e321134964
BjornS2006 <24074918@student.hhs.nl>
2026-01-10 11:38:49 +0100

    Veilingmeester product verwijderen gefixed
    er was een probleem dat als je probeerde een product te verwijderen met een foto, dan kon het product niet verwijderd worden omdat de foto niet eerst verwijderd was. Dat is nu gefixed.

---
+++ RoyalFlora/Migrations/20260110103153_EenFotoPerProduct.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoyalFlora.Migrations
+{
+    /// <inheritdoc />
+    public partial class EenFotoPerProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Fotos_IdProduct",
+                table: "Fotos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fotos_IdProduct",
+                table: "Fotos",
+                column: "IdProduct",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Fotos_IdProduct",
+                table: "Fotos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fotos_IdProduct",
+                table: "Fotos",
+                column: "IdProduct");
+        }
+    }
+}
